fca20d3fcd246c57641e09c617ad503ad1d4e000
Patri <uo282210@uniovi.es>
2023-03-24 21:55:40 +0100

    SDI-2223-304-8.3-Subida a ficheros y gestión de colecciones.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require("mongodb");
 module.exports = function (app, songsRepository) {
 
     // PETICION GET
@@ -24,7 +25,7 @@ module.exports = function (app, songsRepository) {
 
     // *** DEVUELVE FORMULARIO PARA AÑADIR NUEVAS CANCIONES ***
     app.get('/songs/add', function (req, res) {
-        res.render("add.twig");
+        res.render("songs/add.twig");
     });
 
     // PETICION POST
@@ -37,10 +38,56 @@ module.exports = function (app, songsRepository) {
         songsRepository.insertSong(song, function (songId){
            if (songId == null)
                res.send("Error al insertar cancion");
-           else
-               res.send("Agregada la cancion con ID: " + songId)
+           else {
+               if (req.files != null) {
+                   let imagen = req.files.cover;
+                   imagen.mv(app.get("uploadPath") + '/public/covers/' + songId + '.png', function (err) {
+                       if (err) {
+                           res.send("Error al subir la portada de la canción")
+                       } else {
+                           if (req.files.audio != null) {
+                               let audio = req.files.audio;
+                               audio.mv(app.get("uploadPath") + '/public/audios/' + songId + '.mp3', function (err) {
+                                   if (err) {
+                                       res.send("Error al subir el audio");
+                                   } else {
+                                       res.send("Agregada la canción ID: " + songId);
+                                   }
+                               });
+                           }
+                       }
+                   })
+               } else {
+                   res.send("Agregada la canción ID: " + songId)
+               }
+           }
         });
     })
+
+    app.get('/shop', function (req, res){
+       let filter = {};
+       let options = {sort:{title:1}};
+       if (req.query.search != null && typeof(req.query.search) != "undefined" && req.query.search != ""){
+           filter = {"title": {$regex: ".*" + req.query.search + ":*"}};
+       }
+
+       songsRepository.getSongs(filter, options).then(songs => {
+           res.render("shop.twig", {songs: songs})
+       }) .catch(error => {
+           res.send("Se ha producido un error al listar las canciones: " + error)
+       });
+    });
+
+    app.get('/songs/:id', function(req, res){
+       let filter = {_id: ObjectID(req.params.id)};
+       let options = {};
+
+       songsRepository.findSong(filter, options).then(song => {
+           res.render("songs/song.twig", {song:song});
+       }).catch(error => {
+           res.send("Se ha producido un error al buscar la cancion: " + error);
+       });
+    });
 
     // **** PARAMETROS EN URL CON /VALOR ****
     app.get('/songs/:id', function(req, res) {
