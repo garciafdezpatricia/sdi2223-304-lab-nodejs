ba3f4c46df8cb3279de2db675eb43a1faecfabf2
Patri <uo282210@uniovi.es>
2023-03-24 10:36:11 +0100

    SDI–IDGIT-7.304-Ejercicio complementario 2 y 3

--- routes/authors.js
+++ routes/authors.js
@@ -3,7 +3,9 @@ module.exports = function (app){
 
     // *** DEVUELVE FORMULARIO PARA AÑADIR NUEVOS AUTORES ***
     app.get('/authors/add', function (req, res) {
-        res.render("authors/add.twig");
+        let roles = ["bateria", "guitarrista", "bajista", "cantante", "pianista"]
+        let response = {roles: roles}
+        res.render("authors/add.twig", response);
     });
 
 
@@ -50,18 +52,60 @@ module.exports = function (app){
         else
             response += 'Nombre: Nombre no enviado en la peticion <br>'
         // check if the group is defined
-        if (req.body.grupo != null && req.body.grupo.trim() != "" && typeof (req.body.grupo) != "undefined")
+        if (req.body.grupo !== null && req.body.grupo.trim() !== "" && typeof (req.body.grupo) !== "undefined")
             response += 'Grupo: ' + req.body.grupo + '<br>';
         else
             response += 'Grupo: Grupo no enviado en la peticion <br>'
         // check if the rol is defined
-        if (req.body.rol != null && req.body.rol.trim() != "" && typeof (req.body.rol) != "undefined")
+        if (req.body.rol !== null && req.body.rol.trim() !== "" && typeof (req.body.rol) !== "undefined")
             response += 'Rol: ' + req.body.rol;
         else
             response += 'Rol: Rol no enviado en la peticion'
 
         res.send(response);
     })
+
+    app.get('/authors/filter/:rol', function(req, res){
+        // get the rol to filter
+        let requestedRol = req.params.rol;
+
+        // hardcoded list of authors
+        let authors = [{
+            "name":"Jose",
+            "grupo":"Grupo1",
+            "rol":"Cantante"
+        }, {
+            "name":"Antonio",
+            "grupo":"Grupo1",
+            "rol":"Bateria"
+        },{
+            "name":"Miguel",
+            "grupo":"Grupo1",
+            "rol":"Guitarrista"
+        }, {
+            "name":"Patricia",
+            "grupo":"Grupo1",
+            "rol":"Pianista"
+        }, {
+            "name":"Maria",
+            "grupo":"Grupo2",
+            "rol":"Cantante"
+        }, {
+            "name":"Rita",
+            "grupo":"Grupo1",
+            "rol":"Bajista"
+        }];
+
+        let filteredAuthors = authors.filter(author => author.rol.trim().toLowerCase()
+            === requestedRol.trim().toLowerCase())
+
+        let response = {
+            authors: filteredAuthors
+        }
+
+        res.render("authors/authors.twig", response);
+
+    });
 
     app.get('/authors/*', function (req, res) {
         res.redirect("/authors/");
