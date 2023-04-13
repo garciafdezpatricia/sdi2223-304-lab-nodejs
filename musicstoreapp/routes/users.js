19c3128f6198e5f1b5a40fbb535e25818c7524e1
Patri <uo282210@uniovi.es>
2023-04-13 17:32:39 +0200

    SDI-2223-304-9.3-Mensajes y alertas

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -13,9 +13,11 @@ module.exports = function (app, usersRepository) {
       password: securePassword
     }
     usersRepository.insertUser(user).then(userId => {
-      res.redirect('/users/login')
+      res.redirect('/users/login' + "?message=Nuevo usuario registrado"
+          + "&messageType=alert-info");
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      res.redirect('/users/signup' + "?message=Se ha producido un error al registrar el usuario"
+          + "&messageType=alert-danger");
     });
   });
 
@@ -34,7 +36,8 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if (user == null){
         req.session.user = null;
-        res.send("Usuario no identificado");
+        res.redirect('/users/login' + "?message=Email o password incorrecto" +
+            "&messageType=alert-danger");
       }
       else{
         req.session.user = user.email;
@@ -42,7 +45,8 @@ module.exports = function (app, usersRepository) {
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario: " + error);
+      res.redirect('/users/login' + "?message=Email o password incorrecto" +
+          "&messageType=alert-danger");
     })
   })
 
